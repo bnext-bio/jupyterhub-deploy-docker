3d497de7d02a94253b0f8faa9e3f22e8e1f678f4
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:26:24 -0700

    Update template to subdir

--- basic-example/envs/jupyter_server_config_additional.py
+++ basic-example/envs/jupyter_server_config_additional.py
@@ -3,6 +3,6 @@
 
 c.JupyterLabTemplates.allowed_extensions = ["*.ipynb"]
 c.JupyterLabTemplates.template_dirs = ["/home/jovyan/work/templates"]
-c.JupyterLabTemplates.include_default = True
+c.JupyterLabTemplates.include_default = False
 c.JupyterLabTemplates.include_core_paths = True
 c.JupyterLabTemplates.template_label = "Template"
