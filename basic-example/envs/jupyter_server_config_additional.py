507422613cecf1da2bba53801bf6b84921e23e5f
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:22:48 -0700

    Add node and jupyter template config

---
+++ basic-example/envs/jupyter_server_config_additional.py
@@ -0,0 +1,8 @@
+
+# Additional configuration for the nucleus jupyter environment
+
+c.JupyterLabTemplates.allowed_extensions = ["*.ipynb"]
+c.JupyterLabTemplates.template_dirs = ["/home/jovyan/work/templates"]
+c.JupyterLabTemplates.include_default = True
+c.JupyterLabTemplates.include_core_paths = True
+c.JupyterLabTemplates.template_label = "Template"
--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -3,3 +3,4 @@
 echo "Setting up environment"
 /home/jovyan/.local/bin/uv pip install --upgrade nucleus-cdk
 rsync -av /home/jovyan/work-preload/ /home/jovyan/work
+cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
