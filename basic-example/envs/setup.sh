23b1b4adc0385eca4784c5fcba5142500638ffef
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:39:12 -0700

    Move devnote to templates/

--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -6,8 +6,8 @@ rsync -av /home/jovyan/work-preload/ /home/jovyan/work
 cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
 
 # Bring down the curvenote template
-if [ -d ~/work/devnote ]; then 
-    cd ~/work/devnote
+if [ -d ~/work/templates/devnote ]; then 
+    cd ~/work/templates/devnote
     git pull --ff-only 
 else
     git clone https://github.com/antonrmolina/devnote-template.git /home/jovyan/work/templates/devnote
