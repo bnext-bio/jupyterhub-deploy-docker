ac76d1e31e75409c5f027643778d8251e6ebc0f1
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:35:08 -0700

    Bring in devnote template

--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -4,3 +4,6 @@ echo "Setting up environment"
 /home/jovyan/.local/bin/uv pip install --upgrade nucleus-cdk
 rsync -av /home/jovyan/work-preload/ /home/jovyan/work
 cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
+
+# Bring down the curvenote template
+git clone https://github.com/antonrmolina/devnote-template.git /home/jovyan/work/templates/devnote
