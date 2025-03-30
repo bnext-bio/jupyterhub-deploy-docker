4a8b79171e08a9918a990ec66ba279116e10995b
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:41:55 -0700

    Move out of templates because it messes up jupyter_template

--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DEVNOTE_PATH=/home/jovyan/work/devnotes/template
+
 echo "Setting up environment"
 /home/jovyan/.local/bin/uv pip install --upgrade nucleus-cdk
 rsync -av /home/jovyan/work-preload/ /home/jovyan/work
 cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
 
 # Bring down the curvenote template
-if [ -d ~/work/templates/devnote ]; then 
-    cd ~/work/templates/devnote
+if [ -d ${DEVNOTE_PATH} ]; then 
+    cd ${DEVNOTE_PATH}
     git pull --ff-only 
 else
-    git clone https://github.com/antonrmolina/devnote-template.git /home/jovyan/work/templates/devnote
+    git clone https://github.com/antonrmolina/devnote-template.git ${DEVNOTE_PATH}
 fi
