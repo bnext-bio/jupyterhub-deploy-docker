2e6b2f850ce86990a549684e195c0c3eff9d76c3
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:54:38 -0700

    Hide templates

--- basic-example/envs/jupyter_server_config_additional.py
+++ basic-example/envs/jupyter_server_config_additional.py
@@ -2,7 +2,7 @@
 # Additional configuration for the nucleus jupyter environment
 
 c.JupyterLabTemplates.allowed_extensions = ["*.ipynb"]
-c.JupyterLabTemplates.template_dirs = ["/home/jovyan/work/templates"]
+c.JupyterLabTemplates.template_dirs = ["/home/jovyan/work/.templates"]
 c.JupyterLabTemplates.include_default = False
 c.JupyterLabTemplates.include_core_paths = True
 c.JupyterLabTemplates.template_label = "Template"
