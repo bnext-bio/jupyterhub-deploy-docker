2b076560da411550cb3207659634acff3682bc8f
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 16:51:14 -0700

    Autolaunch kernel

--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+JUPYTER_SETTINGS=/opt/conda/share/jupyter/lab/settings
 DEVNOTE_PATH=/home/jovyan/work/devnotes/template
 
 echo "Setting up environment"
 /home/jovyan/.local/bin/uv pip install --upgrade nucleus-cdk
 rsync -av /home/jovyan/work-preload/ /home/jovyan/work
 cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
+
+mkdir -p ${JUPYTER_SETTINGS}
+cp /opt/nucleus-envs/overrides.json ${JUPYTER_SETTINGS}/overrides.json
 
 # Bring down the curvenote template
 if [ -d ${DEVNOTE_PATH} ]; then 
