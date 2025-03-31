2df83b03cff8f229397169c2023fdf9f15f85e8b
Anton Jackson-Smith <anton@dyingstar.net>
2025-03-30 17:01:14 -0700

    Directly update work-preload from git

--- basic-example/envs/setup.sh
+++ basic-example/envs/setup.sh
@@ -5,16 +5,24 @@ DEVNOTE_PATH=/home/jovyan/work/devnotes/template
 
 echo "Setting up environment"
 /home/jovyan/.local/bin/uv pip install --upgrade nucleus-cdk
-rsync -av /home/jovyan/work-preload/ /home/jovyan/work
+
 cat /opt/nucleus-envs/jupyter_server_config_additional.py >> /home/jovyan/.jupyter/jupyter_server_config.py
 
 mkdir -p ${JUPYTER_SETTINGS}
 cp /opt/nucleus-envs/overrides.json ${JUPYTER_SETTINGS}/overrides.json
+
+# Bring down and update our baseline work directory
+if [ -d /home/jovyan/repo ]; then
+    rm -rf /home/jovyan/repo;
+fi
+
+git clone --depth=1 git@github.com:bnext-bio/jupyterhub-deploy-docker.git /home/jovyan/repo
+rsync -av /home/jovyan/repo/basic-example/work-preload/ /home/jovyan/work
 
 # Bring down the curvenote template
 if [ -d ${DEVNOTE_PATH} ]; then 
     cd ${DEVNOTE_PATH}
     git pull --ff-only 
 else
-    git clone https://github.com/antonrmolina/devnote-template.git ${DEVNOTE_PATH}
+    git clone --depth=1 https://github.com/antonrmolina/devnote-template.git ${DEVNOTE_PATH}
 fi
