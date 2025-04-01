6cb1cbf49b6728f458a2a90774a97701fda08d2a
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-01 16:51:46 -0700

    Fix linking

--- basic-example/envs/curvenote-build.sh
+++ basic-example/envs/curvenote-build.sh
@@ -2,17 +2,17 @@
 
 for devnote_dir in `ls -1d ~/work/devnotes/*/`; do
     name=`basename $devnote_dir`
+   
+    export BASE_URL=${BASE_URL_PREFIX}/$name/
+    
+    cd $devnote_dir
+    curvenote build --html
+    
     if [ ! -L ~/work/devnotes/.html/$name ]; then
-        if [ -d ~/devnote_dir/_build/html ]; then
+        if [ -d $devnote_dir/_build/html ]; then
             ln -s $devnote_dir/_build/html ~/work/devnotes/.html/$name
         fi
     fi
-    
-    cd $devnote_dir
-
-    export BASE_URL=${BASE_URL_PREFIX}/$name/
-
-    curvenote build --html
 done
 
 # Kill webserver
